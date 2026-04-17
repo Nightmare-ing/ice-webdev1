0ce51584a6ea286e20cd03a682d6f5c1f30382b7
Qingsong Hu <kingsanhu@gmail.com>
2026-04-17 23:19:52 +0800

    Finish part 1 of the practice

--- starter/src/components/Recipe.jsx
+++ starter/src/components/Recipe.jsx
@@ -2,7 +2,12 @@ import { useState } from "react";
 import { Button, Card } from "react-bootstrap";
 
 export default function Recipe(props) {
-    return <div>
-        <p>I am a recipe!</p>
-    </div>
+    return (
+        <div>
+            <img src={props.img.location} alt={props.img.description} />
+            <h2>{props.name}</h2>
+            <p>by {props.author}</p>
+            <p>{props.keywords}</p>
+        </div>
+    );
 }
