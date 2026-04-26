9d75af9dfca2a485c36840c4210b579f180510b9
Qingsong Hu <kingsanhu@gmail.com>
2026-04-26 22:42:03 +0800

    Finish part 2 of the practice

--- starter/src/components/Recipe.jsx
+++ starter/src/components/Recipe.jsx
@@ -2,12 +2,20 @@ import { useState } from "react";
 import { Button, Card } from "react-bootstrap";
 
 export default function Recipe(props) {
+    const [likes, setLikes] = useState(0);
+
+    function handleLike() {
+        setLikes((old) => old + 1);
+    }
+
     return (
         <div>
             <img src={props.img.location} alt={props.img.description} />
             <h2>{props.name}</h2>
             <p>by {props.author}</p>
             <p>{props.keywords}</p>
+            <p>{likes} likes</p>
+            <Button onClick={handleLike}>Like this Recipe</Button>
         </div>
     );
 }
